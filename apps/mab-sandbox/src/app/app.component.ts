3d07503b6407926229eb856a74eac1d987c95815
Paul Spears <paul.spears@oasisdigital.com>
2020-10-14 09:58:05 -0500

    initial commit

---
+++ apps/mab-sandbox/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mat-abstractions-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'mab-sandbox';
+}
