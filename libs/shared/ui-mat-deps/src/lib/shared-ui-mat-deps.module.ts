c0512714f6863d44ae3a7171d59fdac0f940b03e
Paul Spears <paul.spears@oasisdigital.com>
2020-10-15 12:03:33 -0500

    Cards are climbing abstraction layers

--- libs/shared/ui-mat-deps/src/lib/shared-ui-mat-deps.module.ts
+++ libs/shared/ui-mat-deps/src/lib/shared-ui-mat-deps.module.ts
@@ -4,8 +4,9 @@ import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { DragDropModule } from '@angular/cdk/drag-drop';
+import { MatIconModule } from '@angular/material/icon';
 
-const deps = [DragDropModule, MatButtonModule, MatCardModule];
+const deps = [DragDropModule, MatButtonModule, MatCardModule, MatIconModule];
 
 @NgModule({
   imports: [CommonModule, ...deps],
