be47ea7a1cbb87de0d45d184922394d052ae8357
Paul Spears <paul.spears@oasisdigital.com>
2021-01-05 11:29:45 -0600

    add app-framer example

--- libs/layout-demos/feature/src/lib/layout-demos-feature.module.ts
+++ libs/layout-demos/feature/src/lib/layout-demos-feature.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
+
+import { SharedUiComponentKitModule } from '@mat-abstractions/shared/ui-component-kit';
+
 import { LayoutDemosComponent } from './layout-demos/layout-demos.component';
 
 const routes: Routes = [{ path: '', component: LayoutDemosComponent }];
 
 @NgModule({
-  imports: [CommonModule, RouterModule.forChild(routes)],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes),
+    SharedUiComponentKitModule,
+  ],
   declarations: [LayoutDemosComponent],
 })
 export class LayoutDemosFeatureModule {}
